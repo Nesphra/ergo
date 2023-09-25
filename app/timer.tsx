70a8abd950b2956af15eff705c2914a718e2a3e6
Nesphra <gakills12@gmail.com>
2023-09-25 10:21:31 -0500

    branck trying

--- app/timer.tsx
+++ app/timer.tsx
@@ -1,42 +1,22 @@
 import React, { useEffect, useState } from 'react';
 
-interface State {
-    time: number;
-    seconds: number;
-    minutes: number;
-}
-
 const Timer = () => {
     debugger;
-    const [state, setState] = useState<State>({
-        time: 1500,
-        seconds: 0,
-        minutes: 25,
-    });
+    const [time, setState] = useState<number>();
 
     useEffect(() => {
-        setTimeout(() => {
-            if (state.time === 0){
-                return;
-            }
-
-            setState({
-                time: state.time - 1,
-                minutes: Math.floor((state.time - 1)/ 60),
-                seconds: state.time - Math.floor((state.time - 1)/60) * 60 -1,
-            });
-        }, 1000);
-    }, [state.time]);
+        var time: number = 1500
+        setInterval(() => {
+            time = time - 1;
+            setState(time)
+        }, 1000)
+    }, []);
 
     return (
         <div className="Pomodoro">
-            <button>Focus</button>
-            <button>Short Break</button>
-            <button>Long Break</button>
-            <h1>{`${state.minutes}:${
-                state.seconds <= 10 ? `0${state.seconds}`: state.seconds
-            }`}</h1>
-            <button id="button">play</button>
+            <button>Play</button>
+            <button>Pause</button>
+            <h1>{`${time}`}</h1>
         </div>
     );
 };
