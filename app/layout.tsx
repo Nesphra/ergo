1e69907eff0c4f1f0ea7eef3e510c7abc36c0942
Romeo Beaurain <beaurainr@awty.org>
2023-09-24 23:54:57 -0500

    changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +17,21 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+          <nav className="navBar">
+            <p id="welcome" className='welcome'>
+              Welcome, Romeo.
+            </p>
+            <Link href='/'>
+              Home
+            </Link>
+            <Link href='/notes'>
+              Notes
+            </Link>
+            <Link href='/tasks'>Tasks</Link>
+          </nav>
+        {children}
+      </body>
     </html>
   )
 }
