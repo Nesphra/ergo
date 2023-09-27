d6aecf97adea4fb16f5aa89a89394989d08af841
Nesphra <gakills12@gmail.com>
2023-09-27 13:49:12 -0500

    pomodoro timer fully functional

--- app/_components/timer.tsx
+++ app/_components/timer.tsx
@@ -1,15 +1,23 @@
 import './timer.css'
-import { FaUndoAlt } from 'react-icons/fa';
+import { FaUndoAlt, FaStepForward } from 'react-icons/fa';
 import { FaGear } from 'react-icons/fa6';
 import React, { useEffect, useState } from 'react';
+import { Session } from 'inspector';
 
 const Timer = () => {
-    const [isPaused, setPause] = useState<boolean>(true);
-    const [time, setTime] = useState<number>(1500); // Initial time is 25 minutes
-    const [minutes, setMinutes] = useState<number>(Math.floor(time / 60));
-    const [seconds, setSeconds] = useState<string>(String(time % 60));
 
-    const [pausePlay, setPausePlay] = useState<string>("Play");
+    var pomodoroTime : number = 1500 // 25 minutes, default value = 1500
+    var longBreak : number = 900 // 15 minutes, default value = 900
+    var shortBreak : number = 300 // 5 minutes, default value = 300
+
+    const [isPaused, setPause] = useState<boolean>(true);
+    const [time, setTime] = useState<number>(pomodoroTime); // 25 minutes, default value = 1500
+    const [minutes, setMinutes] = useState<number>(Math.floor(time / 60));
+    
+    const [seconds, setSeconds] = useState<string>(String(time % 60 < 10 ? `0${time % 60}` : time % 60));
+
+    const [sessionCounter, setSessionCounter] = useState<number>(1); // counter of sessions
+    const [pausePlay, setPausePlay] = useState<string>("Play"); // Pause / play word on the button
 
     useEffect(() => {
         let intervalId: NodeJS.Timeout | null = null;
@@ -18,12 +26,17 @@ const Timer = () => {
             intervalId = setInterval(() => {
                 setTime((prevTime) => {
                     if (prevTime <= 0) {
+                        setSessionCounter(sessionCounter + 0.5);
                         clearInterval(intervalId as NodeJS.Timeout);
-                        if (time === 0) {
-                            // If 25-minute timer reaches 0, start a new 5-minute timer
-                            return 300; // 5 minutes in seconds
+                        if (sessionCounter % 1 === 0) { //when the time reaches 0 and the session counter is a full number, put break
+                            if (sessionCounter % 4 == 0){
+                                return longBreak + 1 // the + 1 is for formatting
+                            } else {
+                                return shortBreak + 1 
+                            }
+                        } else { // if the session counter is not a full number, that means you go back to work!
+                            return pomodoroTime + 1 
                         }
-                        return 0;
                     }
                     const newTime = prevTime - 1;
                     setMinutes(Math.floor(newTime / 60));
@@ -33,7 +46,7 @@ const Timer = () => {
             }, 1000);
         } else {
             if (intervalId) {
-                clearInterval(intervalId);
+                clearInterval(intervalId); // when the timer is paused, destroy the interval function
             }
         }
 
@@ -42,9 +55,10 @@ const Timer = () => {
                 clearInterval(intervalId);
             }
         };
-    }, [isPaused, time]);
 
-    const startTimer = () => {
+    }, [isPaused, time, sessionCounter]);
+
+    const startTimer = () => { // set the wording in the pause/ play button
         setPause(!isPaused);
         if (isPaused){
             setPausePlay("Pause")
@@ -53,14 +67,30 @@ const Timer = () => {
         }
     };
 
+    const resetAll = () =>{ // reset the timer when pressed and the session numbers
+        setSessionCounter(1);
+        setTime(pomodoroTime + 1);
+    }
+
+    const skipTimer = () =>{ // skips the current session to go to the next one
+        setTime(0);
+    }
+
     return (
         <div className="Pomodoro">
+            <div className='breaks'>
+                <button>Pomodoro</button>
+                <button>Short Break</button>
+                <button>Long Break</button>
+            </div>
             <h1 className='timer'>{`${minutes}:${seconds}`}</h1>
+            <button className="skipTimer" onClick={skipTimer}><FaStepForward/></button>
             <div className='plReSe'>
                 <button className='buttonPlay' onClick={startTimer}>{`${pausePlay}`}</button>
-                <button className='restart'><FaUndoAlt/></button>
+                <button className='restart' onClick={resetAll}><FaUndoAlt/></button>
                 <button className='settings'><FaGear/></button>
             </div>
+            <p className="sessionCounter">{`#${sessionCounter}`}</p>
         </div>
     );
 };
